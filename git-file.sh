e8d5ab4ef682edaac5ee071d8545fcb8748adeee
Rohit Marathe <rohitmarathe7989@gmail.com>
2023-03-10 18:10:43 +0530

    shell script is added

---
+++ git-file.sh
@@ -0,0 +1,9 @@
+sudo apt update  -y
+sudo apt upgrade  -y
+sudo apt install git -y
+sudo apt install gitsome -y
+sudo apt install gh -y
+sudo apt install zsh -y
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo "exec zsh" >  ~/.bashrc
+
