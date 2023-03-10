96893126cd7c0ac135ff6a2c97b9799960569d52
Rohit Marathe <rohitmarathe7989@gmail.com>
2023-03-10 18:29:44 +0530

    shell script

--- git-file.sh
+++ git-file.sh
@@ -4,6 +4,7 @@ sudo apt install git -y
 sudo apt install gitsome -y
 sudo apt install gh -y
 sudo apt install zsh -y
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
 echo "exec zsh" >  ~/.bashrc
+gh auth login
 
